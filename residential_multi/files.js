3fd30e302100a5e9bc7333a82ab5ae5a4afb46dc
maximtsai <maximtsai@yahoo.com>
2025-01-18 10:07:18 -0800

    almost all

---
+++ residential_multi/files.js
@@ -0,0 +1,50 @@
+var projects = [
+	[
+		{url: "hebei_apartment/hebei_renqiu yuhui_1.jpg", 
+			text1: "Hebei Renqiu Yuhui (Hui Zhan Dao) Retail/Office/Apartment Complex", 
+			text2: "Site Area: 20,700 SM Building Area: 162,000 SM", 
+			text3: "Positioning Company: Beijing Qing Gu Yin Xiang Environmental Planning and Design"},
+		{url: "hebei_apartment/hebei_renqiu yuhui_2.jpg", 
+			text1: "Hebei Renqiu Yuhui Retail/Office/Apartment Complex", 
+			text2: "", 
+			text3: ""},
+		{url: "hebei_apartment/hebei_renqiu yuhui_3.jpg", 
+			text1: "Hebei Renqiu Yuhui Retail/Office/Apartment Complex", 
+			text2: "", 
+			text3: ""},
+		{url: "hebei_apartment/hebei_renqiu yuhui_4.jpg", 
+			text1: "Hebei Renqiu Yuhui Retail/Office/Apartment Complex", 
+			text2: "", 
+			text3: ""},
+		{url: "hebei_apartment/hebei_renqiu yuhui_5.jpg", 
+			text1: "Hebei Renqiu Yuhui Retail/Office/Apartment Complex", 
+			text2: "", 
+			text3: ""}
+	], [
+		{url: "sparta_apartment/sparta_apartment_3.jpg", 
+			text1: "SPARTA Apartment Building", 
+			text2: "",
+			text3: ""},
+		{url: "sparta_apartment/sparta_apartment_1.jpg", 
+			text1: "SPARTA Apartment Building", 
+			text2: "Location: San Jose, CA", 
+			text3: ""},
+		{url: "sparta_apartment/sparta_apartment_2.jpg", 
+			text1: "SPARTA Apartment Building", 
+			text2: "",
+			text3: ""}
+	], [
+		{url: "courtyard_townhomes/13.jpg", 
+			text1: "Misc Courtyard Townhomes", 
+			text2: "",
+			text3: ""},
+		{url: "courtyard_townhomes/12.jpg", 
+			text1: "Misc Courtyard Townhomes", 
+			text2: "",
+			text3: ""}
+	]
+]
+
+projects[0].title="Hebei Apartment Complex";
+projects[1].title="SPARTA Apartment Building";
+projects[2].title="Courtyard Townhomes";
