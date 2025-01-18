d2488f404797633c1e5f282dd65e5841b3daf4a3
maximtsai <maximtsai@yahoo.com>
2025-01-18 10:06:54 -0800

    more stuff

---
+++ designconsulting/files.js
@@ -0,0 +1,31 @@
+var projects = [
+	[
+		{url: "novado/CENTRAL_GREEN_PERSPECTIVE.jpg", 
+			text1: "Novado Design Consulting Project", 
+			text2: "Central Green Perspective", 
+			text3: ""},
+		{url: "novado/CENTRAL_TOWNHOME_FRONT_ELEVATION.jpg", 
+			text1: "Novado Design Consulting Project", 
+			text2: "Front Elevation", 
+			text3: ""},
+	
+	],
+	[
+		{url: "novado/ENCINAL_PERSPECTIVE_1.jpg", 
+			text1: "ENCINAL Design Consulting Project", 
+			text2: "Perspective 1", 
+			text3: ""},
+		{url: "novado/ENCINAL_PERSPECTIVE_2.jpg", 
+			text1: "ENCINAL Design Consulting Project", 
+			text2: "Perspective 2", 
+			text3: ""},
+		{url: "novado/ENCINAL_PERSPECTIVE_3.jpg", 
+			text1: "ENCINAL Design Consulting Project", 
+			text2: "Perspective 3", 
+			text3: ""},
+
+	]
+]
+
+projects[0].title="Novado Design Consulting Project";
+projects[1].title="ENCINAL Design Consulting Project";
